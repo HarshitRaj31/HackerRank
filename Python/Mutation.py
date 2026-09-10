aff566860a911ef7392204c8035acfee139d2393
Harshit Raj <iamharshit106@gmail.com>
2026-09-10 10:49:41 +0530

    Uploading

--- Python/Mutation.py
+++ Python/Mutation.py
@@ -1,5 +1,5 @@
 def mutate_string(string, position, character):
-    string =string[:5]+"k"+string[6:]
+    string =string[:position]+character+string[position+1:]
     return string
 
 if __name__ == '__main__':
